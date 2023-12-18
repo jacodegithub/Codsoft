d59e18504e00a9dedfb209ff4a658e8525b8c516
jacodegithub <dudeobama246@gmail.com>
2023-12-18 22:34:24 +0530

    commit: updating

--- LEVEL-2/TASK-1/routes/authRoutes.js
+++ LEVEL-2/TASK-1/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 // Auth routes
-router.get('/', (res, req) => {
-    res.send('Hello from Waqhar')
+router.get('/', (req, res) => {
+    res.send("Hello from Waqhar!")
 })
 
 module.exports = router;
